c2b4e91806e233fcce34a155c382af563d8cec5d
Patrick Fic <patrick@thinkimex.com>
2019-10-21 14:54:04 -0700

    Missed files

---
+++ src/homepage.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './homepage.styles.scss'
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="conent-container">
+          <h1 className="title">Hats</h1>
+          <span className="substitle">Shop Now</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
