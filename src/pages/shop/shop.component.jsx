3784abf581090439a1398dfeeea21eadc4699cb4
Patrick Fic <pfic@outlook.com>
2019-10-23 10:36:12 -0700

    added shop page and components

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import ShopData from "./shop.data";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+
+export default class ShopPage extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      collections: ShopData
+    };
+  }
+
+  render() {
+      const {collections} = this.state
+    return (
+      <div className='shop-page'>
+      {collections.map(({id, ...otherCollectionProps}) => (
+        <CollectionPreview key = {id} {...otherCollectionProps} />
+      ))}
+      </div>
+    );
+  }
+}
