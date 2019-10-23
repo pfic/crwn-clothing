d1c693b9c695e98f53ef5562e2d51d4ec6fd2594
Patrick Fic <pfic@outlook.com>
2019-10-23 14:35:36 -0700

    Header and links

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import './header.styles.scss'
+import {ReactComponent as Logo} from '../../assets/crown.svg'
+
+
+export default function Header() {
+    return (
+      <div className="header">
+        <Link to="/">
+          <Logo className="logo-container" />>
+        </Link>
+        <div className="options">
+          <Link className='option' to='/shop'>Shop</Link>
+          <Link className='option' to='/contact'>Contact</Link>
+        </div>
+      </div>
+    );
+}
