8fadab7483d86432c516ccf5076fe532318c7fe6
Patrick Fic <patrick@thinkimex.com>
2019-10-29 09:53:17 -0700

    In progress login work.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -18,20 +18,31 @@ class App extends React.Component {
 
   unsubscribeFromAuth = null;
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapshot =>
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          })
+        );
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
-componentWillUnmount(){
-  this.unsubscribeFromAuth();
-}
-  
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,28 +1,30 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import './header.styles.scss'
-import {ReactComponent as Logo} from '../../assets/crown.svg'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./header.styles.scss";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
 
-export default function Header({currentUser}) {
-    return (
-      <div className="header">
-        <Link to="/">
-          <Logo className="logo-container" />
+export default function Header({ currentUser }) {
+  return (
+    <div className="header">
+      <Link to="/">
+        <Logo className="logo-container" />
+      </Link>
+      <div className="options">
+        <Link className="option" to="/shop">
+          Shop
         </Link>
-        <div className="options">
-          <Link className="option" to="/shop">
-            Shop
+        <Link className="option" to="/contact">
+          Contact
+        </Link>
+        {currentUser ? (
+          <div onClick={() => auth.signOut()}>SIGN OUT</div>
+        ) : (
+          <Link className="option" to="/signin">
+            SIGN IN
           </Link>
-          <Link className="option" to="/contact">
-            Contact
-          </Link>
-          {currentUser ? (
-            <div onClick={() => auth.signOut()}>SIGN OUT</div>
-          ) : (
-            <Link className="option" to="/signin">SIGN IN</Link>
-          )}
-        </div>
+        )}
       </div>
-    );
+    </div>
+  );
 }
