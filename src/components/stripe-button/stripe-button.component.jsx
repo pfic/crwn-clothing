159558bff65853ab0882d6fe2d8863f63b162d0b
Patrick Fic <patrick@thinkimex.com>
2019-11-04 13:51:47 -0800

    End of lesson 139. App is relatively completed.

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  //Price must be in cents.
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_GLpE7kUFN8pSpziifyChAqbt00zUWLdIjs";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment succesful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
