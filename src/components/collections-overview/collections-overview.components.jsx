e3116059954c72795334cf815571225114a9a534
Patrick Fic <patrick@thinkimex.com>
2019-11-05 13:46:55 -0800

    Fixed firebase config and committed shop data to database.

--- src/components/collections-overview/collections-overview.components.jsx
+++ src/components/collections-overview/collections-overview.components.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import "./collections-overview.styles.scss";
 import CollectionPreview from "../collection-preview/collection-preview.component";
-import { selectCollectionsForPreview } from "../../redux/shop/shop.selector";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
 
 const CollectionsOverview = ({ collections }) => (
   <div className="collections-overview">
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,15 +1,16 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyD5cPSqn_nwspi2gSIpib4FECIKMQMabcs",
+  authDomain: "crwn-clothing-5ccb2.firebaseapp.com",
+  databaseURL: "https://crwn-clothing-5ccb2.firebaseio.com",
+  projectId: "crwn-clothing-5ccb2",
+  storageBucket: "crwn-clothing-5ccb2.appspot.com",
+  messagingSenderId: "45614455010",
+  appId: "1:45614455010:web:6191c51eb7cd9cccd726bb",
+  measurementId: "G-EGRPXRTHM3"
 };
 
 firebase.initializeApp(config);
@@ -32,18 +33,38 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         ...additionalData
       });
     } catch (error) {
-      console.log('error creating user', error.message);
+      console.log("error creating user", error.message);
     }
   }
 
   return userRef;
 };
 
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+  const batch = firestore.batch();
+
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+  // return await batch.commit();
+  batch
+  .commit()
+  .then(function() {
+    console.log('Done.')
+  })
+  .catch(err => console.log(`There was an error: ${err}`))
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
